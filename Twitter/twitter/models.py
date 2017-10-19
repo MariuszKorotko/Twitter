e8a13420c60cd8d4d931954d087ade9decddfc16
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-19 12:17:18 +0200

    Create model Tweet.

--- Twitter/twitter/models.py
+++ Twitter/twitter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tweet(models.Model):
+    """Stores a single tweet, related to :model:`auth.User`."""
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
