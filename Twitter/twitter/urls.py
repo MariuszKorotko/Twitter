e0799dc5d16489c3002595c3ce39412b42e22c67
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-19 11:18:32 +0200

    First url index.

--- Twitter/twitter/urls.py
+++ Twitter/twitter/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+from .views import index
 
 app_name = 'twitter'
 
 urlpatterns = [
-
+    url(r'^$', index, name='index'),
 ]
