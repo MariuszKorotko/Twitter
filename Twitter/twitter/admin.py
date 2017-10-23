cc52ea225dfcde82a1e718c8f896fd360103d734
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-23 13:11:56 +0200

    Add search field and filter fields

--- Twitter/twitter/admin.py
+++ Twitter/twitter/admin.py
@@ -6,3 +6,5 @@ from .models import Tweet
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('content', 'creation_date', 'user')
     date_hierarchy = 'creation_date'
+    search_fields = ['content']
+    list_filter = ('user__username', 'creation_date')
