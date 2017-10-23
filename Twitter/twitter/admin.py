beecc52e910ea748caa6cd5ea6c5ed7545dcffd2
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-23 12:34:52 +0200

    Creaet TweetAdmin class

--- Twitter/twitter/admin.py
+++ Twitter/twitter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tweet
 
-# Register your models here.
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('content', 'creation_date', 'user')
+    date_hierarchy = 'creation_date'
