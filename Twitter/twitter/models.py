cd02414e467811f89db942e2a987735d47486a75
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-23 12:34:14 +0200

    Delete unique attribute model:Tweet, field:user.

--- Twitter/twitter/models.py
+++ Twitter/twitter/models.py
@@ -6,4 +6,4 @@ class Tweet(models.Model):
     """Stores a single tweet, related to :model:`auth.User`."""
     content = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
