ff9609308e48a3c1bf8cca2040d890bdd9f8d25a
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-24 13:07:06 +0200

    Change method index on class.

---
+++ twitter/views.py
@@ -0,0 +1,12 @@
+from django.views import generic
+from .models import Tweet
+
+
+class IndexView(generic.ListView):
+    """
+    Display all tweets :model:`twitter.Tweet`
+    """
+    template_name = 'twitter/index.html'
+
+    def get_queryset(self):
+        return Tweet.objects.order_by('creation_date')
