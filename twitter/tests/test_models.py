0e34c54cdd3240db420d68b031643fc71219aaf2
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-24 14:14:47 +0200

    Add create_tweet and test_tweet_creation_method.

--- twitter/tests/test_models.py
+++ twitter/tests/test_models.py
@@ -1,1 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth.models import User
+from twitter.models import Tweet
+
+
+class TwettTest(TestCase):
+    """Testing by creating new tweet using :model:`Tweet`"""
+    def create_tweet(self, content="Test Tweet!", creation_date=timezone.now(
+
+    ), user=User()):
+        user.save()
+        return Tweet.objects.create(content=content,
+                                    creation_date=creation_date, user=user)
+
+    def test_tweet_creation(self):
+        """Create tweet"""
+        tweet = self.create_tweet()
+        self.assertTrue(isinstance(tweet, Tweet))
+        self.assertEqual(tweet.content, "Test Tweet!")
+        self.assertEqual(tweet.creation_date, tweet.creation_date)
+        self.assertEqual(tweet.user, tweet.user)
