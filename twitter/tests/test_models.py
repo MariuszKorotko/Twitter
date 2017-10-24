9d78738b67c123492b98f3b5c245225e94ee42a0
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-24 15:27:21 +0200

    Change mehtod create_tweet on setUp method.

--- twitter/tests/test_models.py
+++ twitter/tests/test_models.py
@@ -6,17 +6,15 @@ from twitter.models import Tweet
 
 class TweetModelTest(TestCase):
     """Testing by creating new tweet using :model:`Tweet`"""
-    def create_tweet(self, content="Test Tweet!", creation_date=timezone.now(
-
-    ), user=User()):
+    def setUp(self):
+        user = User()
         user.save()
-        return Tweet.objects.create(content=content,
-                                    creation_date=creation_date, user=user)
+        Tweet.objects.create(content="Test Tweet",
+                             creation_date=timezone.now(), user=user)
+
 
     def test_tweet_creation(self):
         """Create and test tweet"""
-        tweet = self.create_tweet()
+        tweet = Tweet.objects.get(content="Test Tweet")
         self.assertTrue(isinstance(tweet, Tweet))
-        self.assertEqual(tweet.content, "Test Tweet!")
-        self.assertEqual(tweet.creation_date, tweet.creation_date)
-        self.assertEqual(tweet.user, tweet.user)
+        self.assertEqual(tweet.content, "Test Tweet")
