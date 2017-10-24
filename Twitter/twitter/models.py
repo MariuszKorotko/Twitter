554fc7560367edfc2ab3794807da988f09bd68fe
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-24 13:19:24 +0200

    Delete duplicate files and directories.

--- Twitter/twitter/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Tweet(models.Model):
-    """Stores a single tweet, related to :model:`auth.User`."""
-    content = models.TextField()
-    creation_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
