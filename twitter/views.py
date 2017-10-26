25e53b4fdaec355526019a435da8965ae58c7c53
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-26 10:48:55 +0200

    Changes

--- twitter/views.py
+++ twitter/views.py
@@ -3,7 +3,9 @@ from .models import Tweet
 
 
 class IndexView(generic.ListView):
-    """Display all tweets create by :model:`Tweet` model."""
+    """Display all tweets using :view:`twitter.IndexView` create by
+    :model:`auth.User` using model :model:`twitter.Tweet`.
+    """
     template_name = 'twitter/index.html'
 
     def get_queryset(self):
