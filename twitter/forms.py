9ac7694139ad61ee86477a5a8bb3d557bbe299fa
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-10-26 11:14:17 +0200

    Add forms.py and AddNewTweetForm.

---
+++ twitter/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from django.forms.widgets import Textarea
+from twitter.models import Tweet
+
+
+class AddNewTweetForm(ModelForm):
+    """Adding new tweet using Model Form."""
+    class Meta:
+        model = Tweet
+        fields = ['content']
+        widgets = {
+            'content': Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
