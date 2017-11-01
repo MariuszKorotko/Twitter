3cc4e97fc98bd402e057c71a6d0509dd0da38398
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-11-01 10:11:09 +0100

    Change user field on settings.AUTH_USER_MODEL.

--- twitter/models.py
+++ twitter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 
@@ -6,7 +7,8 @@ class Tweet(models.Model):
     """Stores a single tweet, related to :model:`auth.User`."""
     content = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, null=True,
                              blank=True)
 
     def __str__(self):
