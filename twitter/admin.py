886b917380f945a76d1233a512c9894f572c4800
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-11-02 15:11:28 +0100

    Add User model to admin panel.

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-from .models import Tweet
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import User, Tweet
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
 
 
 @admin.register(Tweet)
@@ -7,4 +32,4 @@ class TweetAdmin(admin.ModelAdmin):
     list_display = ('content', 'creation_date', 'user')
     date_hierarchy = 'creation_date'
     search_fields = ['content']
-    list_filter = ('user__username', 'creation_date')
+    list_filter = ('user', 'creation_date')
