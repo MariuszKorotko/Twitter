f69f80ebed405daf2eac95b2789c350dce5d6949
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-12-18 16:55:30 +0100

    Create messages url

--- twitter/urls.py
+++ twitter/urls.py
@@ -46,5 +46,5 @@ urlpatterns = [
     url(r'^password_change/done/$', auth_views.PasswordChangeDoneView.as_view(
             template_name='registration/password_change_done.html'),
         name='password_change_done'),
-    url(r'^messages/$', MessagesView.as_view(), name='messages')
+    url(r'^messages/$', MessagesView.as_view(), name='messages'),
 ]
