2a82e41c562d85e9848059fc8eb4fd64863a9ce8
Mariusz Korotko <mariusz.korotko@wp.pl>
2017-12-20 15:09:51 +0100

    Create block and unblock actions in admin.py

--- twitter/admin.py
+++ twitter/admin.py
@@ -5,6 +5,16 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Comment, Message, User, Tweet
 
 
+def change_on_blocked(model_admin, request, query_set):
+    """Change blocked attribute to True"""
+    query_set.update(blocked=True)
+
+
+def change_on_unblock(model_admin, request, query_set):
+    """Change blocked attribute to True"""
+    query_set.update(blocked=False)
+
+
 @admin.register(User)
 class UserAdmin(UserAdmin):
     """Define admin model for custom User model with no email field."""
@@ -29,24 +39,27 @@ class UserAdmin(UserAdmin):
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ('contents', 'creation_date', 'user')
+    list_display = ('contents', 'creation_date', 'user', 'blocked')
     date_hierarchy = 'creation_date'
     search_fields = ['contents']
-    list_filter = ('user', 'creation_date')
+    list_filter = ('user', 'creation_date', 'blocked')
+    actions = [change_on_blocked, change_on_unblock]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('contents', 'creation_date', 'user')
+    list_display = ('contents', 'creation_date', 'user', 'blocked')
     date_hierarchy = 'creation_date'
     search_fields = ['contents']
-    list_filter = ('user', 'creation_date')
+    list_filter = ('user', 'creation_date', 'blocked')
+    actions = [change_on_blocked, change_on_unblock]
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('contents', 'creation_date', 'sender', 'receiver',
-                    'read_off')
+                    'read_off', 'blocked')
     date_hierarchy = 'creation_date'
     search_fields = ['contents', 'sender', 'receiver']
-    list_filter = ('sender', 'receiver', 'creation_date', 'read_off')
+    list_filter = ('sender', 'receiver', 'creation_date', 'read_off', 'blocked')
+    actions = [change_on_blocked, change_on_unblock]
